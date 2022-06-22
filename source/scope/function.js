3a407636752a47e938477fd59dac5fc82436a858
sisifodev <manuelperdomo1710@icloud.com>
2022-06-22 18:28:38 -0500

    Function scope

---
+++ source/scope/function.js
@@ -0,0 +1,11 @@
+function greeting() {
+  let userName = "John";
+  console.log(userName);
+
+  if (userName === "John") {
+    console.log(`Hello, ${userName}!`);
+  }
+}
+
+greeting();
+console.log(userName);
