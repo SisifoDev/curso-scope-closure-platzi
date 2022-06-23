5d9b3181d1ef590eab06675e914295b0863f0f56
sisifodev <manuelperdomo1710@icloud.com>
2022-06-23 17:39:56 -0500

    Ámbito léxico

---
+++ source/closure/lexical.js
@@ -0,0 +1,20 @@
+const myGlobal = 0;
+
+function myFunction() {
+  const myNumber = 1;
+  console.log(myGlobal);
+
+  function parent() {
+    //Funcion interna
+    const inner = 2;
+    console.log(myNumber, myGlobal);
+    function child() {
+      //Funcion interna
+      console.log(inner, myNumber, myGlobal);
+    }
+    return child();
+  }
+  return parent();
+}
+
+myFunction();
